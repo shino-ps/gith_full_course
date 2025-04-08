e68db2fe66e856ec53741e1732a554736b84314a
shino <s.shinohara@pocosof.co.jp>
2025-04-08 12:31:33 +0900

    色々開発した

---
+++ index1.js
@@ -0,0 +1,34 @@
+git diff
+echo 'console.log("change")' > index1.js
+
+git diff
+git status
+echo 'console.log("change")' > index1.js
+
+git diff
+
+echo 'console.log("change")' >> index1.js
+echo 'console.log("change")' >> index1.js
+git diff
+git status
+
+git attua
+git status
+
+
+
+
+git status
+pwd
+ls -a
+
+pwd
+ls -a
+pwd
+ls -a
+
+cd ~/Documents/git_handson
+git status
+
+pwd
+
